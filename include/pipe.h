325842535aae5c9c0fd4b0370ee94502194bef78
mehmetbosdanci <133037856+mehmetbosdanci@users.noreply.github.com>
2024-12-27 11:54:06 +0300

    pipe.h dosyası eklendi

---
+++ include/pipe.h
@@ -0,0 +1,5 @@
+#ifndef PIPE_H
+#define PIPE_H
+void my_pipe(char *commands[],char *args[],char *input_file,char *output_file,int background,int i);
+//my_pipe(char **, char *, char *, int,  volatile int);
+#endif
